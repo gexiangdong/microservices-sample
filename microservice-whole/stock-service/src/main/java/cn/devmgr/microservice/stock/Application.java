8f65137044fbc7c5a9c96df80035c82b57ef463f
gexiangdong <gexiangdong@clschina.com>
2017-11-30 18:14:24 +0800

    first commit

--- microservice-whole/stock-service/src/main/java/cn/devmgr/microservice/stock/Application.java
+++ microservice-whole/stock-service/src/main/java/cn/devmgr/microservice/stock/Application.java
@@ -1,12 +1,17 @@
-package cn.devmgr;
+package cn.devmgr.microservice.stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
+
+/**
+ * 注意@ComponentScan加入后，会仅仅扫描指定的包，不包含当前包；如果需要，必须指定当前包，否则不会扫描当前包，
+ */
 @SpringBootApplication
 @EnableEurekaClient
+@ComponentScan(basePackages= {"cn.devmgr.common.security", "cn.devmgr.microservice.stock"})
 public class Application {
 
     public static void main(String[] args) throws Exception {
